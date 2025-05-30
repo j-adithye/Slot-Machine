5ff4070235e48ff424201ed11ec2508abd0e6b91
J-Adithye <adithyej8@gmail.com>
2025-05-30 14:15:32 +0530

    looping fix

--- main.py
+++ main.py
@@ -8,11 +8,9 @@ ROWS = 3
 COLS = 3
 
 def run_slot_machine():
+    #generate slot machine
     symbols = [ 'A' , 'B' , 'C' , 'D' , 'E' ]
-    slot_machine = [[0 for _ in range(ROWS)] for _ in range(COLS)]
-    for i in slot_machine:
-        for index, val in enumerate(i):
-            i[index] = random.choice(symbols)
+    slot_machine = [[random.choice(symbols) for _ in range(ROWS)] for _ in range(COLS)]
     return slot_machine
 
 def deposit():
@@ -29,6 +27,7 @@ def deposit():
     return amount
 
 def no_of_lines_input():
+    # How many lines are you betting
     while True:
         lines = input("Enter no of lines (1-"+ str(MAX_LINES) +"): ")
         if lines.isdigit():
@@ -55,44 +54,72 @@ def get_bet():
     return amount
 
 def calc_result(s_m):
+    #Calculate result
     valid_lines = 0
-    val =s_m[0]
     
+    # Horizontal Check
     for row in s_m:
-        for i in row:
+        val = row[0]
+        for index,i in enumerate(row):
             if i != val:
                 break
             else:
-                if i == row[-1]:
+                if index == MAX_LINES-1:
                     valid_lines += 1
-                continue
-            
+    
+    #Vertical_check
+    for row in range(3):
+        last_row = s_m[-1]
+        first_row = s_m[0]
+        val = first_row[row]
+        for col in range(3):
+            if s_m[col][row] != val:
+                break
+            elif val == s_m[col][row] and col == 2:
+                valid_lines += 1
+                    
+                    
     return valid_lines
         
 
 def main():
     wallet = deposit()
-    lines = no_of_lines_input()
-    bet = get_bet()
-    total_bet = bet * lines
     
-    if total_bet < wallet:
-        print(f"Betting ${bet} on {lines} lines. Total bet is ${total_bet}")
-    else:
-        print("Not enough Balance")
-
-    print(f"Balance : {wallet} Lines : {lines} " )
-    
-    print()
-    slot_machine = run_slot_machine()
-    print("Slot Machine Result:")
-    for row in slot_machine:
-        for col in range(MAX_LINES):
-            print(f"| {row[col]} |",end = '')
-        print()
+    while wallet>0:
         
-    result = calc_result(slot_machine)
+        lines = no_of_lines_input()
+        bet = get_bet()
+        total_bet = bet * lines
     
+        if total_bet < wallet:
+            print(f"Betting ${bet} on {lines} lines. Total bet is ${total_bet}")
+        else:
+            print(f"Not enough Balance Your balance is {wallet}")
+
+        wallet = wallet - total_bet
+        print(f"Balance : {wallet} || Lines : {lines} || Total Bet : {total_bet}")
+    
+        print()
+        slot_machine = run_slot_machine()
+        print("Slot Machine Result:")
+        for row in slot_machine:
+            for col in range(MAX_LINES):
+                print(f"| {row[col]} |",end = '')
+            print()
+        
+        result = calc_result(slot_machine)
+        print(f"You won {result} lines")
+        
+        wallet = wallet + result * bet
+        print(f"Updated Balance is {wallet}")
+        
+        proceed = input("Do you want to continue Y/n (Default value is 'n'):")
+        if proceed == "y" or "Y":
+            print(proceed)
+            continue
+        else:
+            break
+        
     
 if __name__ == "__main__":
     main()
